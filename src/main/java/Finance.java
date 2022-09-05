62ac3c99660fde87369be66899c331649a7162c2
zsamadi <zsamadi.snrt@gmail.com>
2022-09-05 23:34:57 +0100

    module6

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -35,34 +35,34 @@ public class Finance {
             SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
             MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>");
 
-    private static boolean validateCommandArguments(String[] args) {
+    private static boolean validateCommandArguments(String[] arguments) {
 
-        switch (args[0]) {
+        switch (arguments[0]) {
             case BEST_LOAN_RATES:
-                return args.length == 1;
+                return arguments.length == 1;
             case SAVINGS_CALCULATOR:
-                return args.length == 3 ;
+                return arguments.length == 3 ;
             case MORTGAGE_CALCULATOR:
-                return args.length == 4 ;
+                return arguments.length == 4 ;
         }
 
     return false;
     }
 
-    private static void executeCommand(String command,String[] args){
+    private static void executeCommand(String command,String[] arguments){
 
         switch(command){
             case BEST_LOAN_RATES:
                 System.out.println("Finding best loan rates ...");
-                BestLoanRates.main(args);
+                BestLoanRates.main(arguments);
                 return;
             case SAVINGS_CALCULATOR:
                 System.out.println("Finding your net savings ...");
-                SavingsCalculator.main(args);
+                SavingsCalculator.main(arguments);
                 return;
             case MORTGAGE_CALCULATOR:
                 System.out.println("Finding your monthly payment ...");
-                MortgageCalculator.main(args);
+                MortgageCalculator.main(arguments);
                 return;
         }
 
